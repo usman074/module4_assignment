98f2314a43483a0724750e30f9aa6ad2f6892d0b
usman074 <usman.iqbal6867@gmail.com>
2020-08-28 19:55:49 +0500

    first commit

---
+++ SpeakHello.js
@@ -0,0 +1,10 @@
+(function(window) {
+  let helloSpeaker = {};
+  let speakWord = "Hello";
+  helloSpeaker.speak = function(name) {
+    console.log(speakWord + " " + name)
+  }
+
+  window.helloSpeaker = helloSpeaker; 
+
+})(window);
---
+++ script.js
@@ -0,0 +1,11 @@
+(function() {
+  let names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  for (let i = 0; i < names.length; i++){
+    let firstLetter = names[i].charAt(0).toLowerCase(); 
+    if(firstLetter == "j"){
+      byeSpeaker.speak(names[i]);
+    } else {
+        helloSpeaker.speak(names[i]);  
+    }
+  }
+})();
